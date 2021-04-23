23826ca3c860c01fa77bce6bfcbbe891afa9f0c1
RaduSoare <radu.soare23@yahoo.com>
2021-04-23 14:20:41 +0300

    Main page design done + link to categories

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const app = express();
 
 app.use("/static", express.static('./static/'));
 
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/main.html'));
 });
 
 app.get('/login', function(req, res) {
     res.sendFile(path.join(__dirname, '/login.html'));
+});
+
+app.get('/categories', function(req, res) {
+    res.sendFile(path.join(__dirname, '/categories.html'));
 });
 
 app.listen(3000);
