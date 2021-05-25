ef917deeefbabbc9737eb46097264fd604072ca1
RaduSoare <radu.soare23@yahoo.com>
2021-05-26 00:20:23 +0300

    port changed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.use("/static", express.static('./static/'));
 
@@ -34,4 +34,4 @@ app.get('/personal-content', function(req, res) {
 });
 
 
-app.listen(3000);
+app.listen(port);
